f68955504919235d634b06135792c8ba1228c157
shohruuh <shohruhkan007@gmail.com>
2025-06-27 19:04:29 +0500

    Initial Commit

---
+++ Rick and Morty/Rick_and_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_and_MortyApp.swift
+//  Rick and Morty
+//
+//  Created by admin on 27/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Rick_and_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
